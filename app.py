ec02b2f69d33c28037db7516e5ce8068ffb2b193
alexblackbean <alexblackbean@gmail.com>
2021-12-31 14:26:09 +0800

    Linebot can accept multiuser

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sys
 
 from flask import Flask, jsonify, request, abort, send_file
 from dotenv import load_dotenv
-from linebot import LineBotApi, WebhookParser
+from linebot import LineBotApi, WebhookParser, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
@@ -12,32 +12,35 @@ from utils import send_text_message
 
 load_dotenv()
 
-
-machine = TocMachine(
-    states=["user", "state1", "state2"],
-    transitions=[
-        {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state1",
-            "conditions": "is_going_to_state1",
-        },
-        {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state2",
-            "conditions": "is_going_to_state2",
-        },
-        {"trigger": "go_back", "source": ["state1", "state2"], "dest": "user"},
-    ],
-    initial="user",
-    auto_transitions=False,
-    show_conditions=True,
-)
+class User():
+    def __init__(self):
+        self.uid = 0
+        self.machine = None
+# machine = TocMachine(
+#     states=["user", "state1", "state2"],
+#     transitions=[
+#         {
+#             "trigger": "advance",
+#             "source": "user",
+#             "dest": "state1",
+#             "conditions": "is_going_to_state1",
+#         },
+#         {
+#             "trigger": "advance",
+#             "source": "user",
+#             "dest": "state2",
+#             "conditions": "is_going_to_state2",
+#         },
+#         {"trigger": "go_back", "source": ["state1", "state2"], "dest": "user"},
+#     ],
+#     initial="user",
+#     auto_transitions=False,
+#     show_conditions=True,
+# )
 
 app = Flask(__name__, static_url_path="")
-
-
+users = []
+first_time = True
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
@@ -50,37 +53,85 @@ if channel_access_token is None:
 
 line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
-
+handler = WebhookHandler(channel_secret)
 
 @app.route("/callback", methods=["POST"])
 def callback():
     signature = request.headers["X-Line-Signature"]
     # get request body as text
     body = request.get_data(as_text=True)
+    # if 'userid'
     app.logger.info(f"Request body: {body}")
-
     # parse webhook body
     try:
         events = parser.parse(body, signature)
+        handler.handle(body,signature)
     except InvalidSignatureError:
         abort(400)
-
     # if event is MessageEvent and message is TextMessage, then echo text
+    # print(len(users))
     for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            continue
-        if not isinstance(event.message.text, str):
-            continue
-        print(f"\nFSM STATE: {machine.state}")
-        print(f"REQUEST BODY: \n{body}")
-        response = machine.advance(event)
-        if response == False:
-            send_text_message(event.reply_token, "Not Entering any State")
+        user_id = event.source.user_id
+        flag = False
+        temp = None
+        for item in users:
+            if item.uid == user_id:
+                flag = True
+                temp = item.machine
+                break
+        # if not isinstance(event, MessageEvent):
+        #     continue
+        # if not isinstance(event.message, TextMessage):
+        #     continue
+        # if not isinstance(event.message.text, str):
+        #     continue
+        # print(f"\nFSM STATE: {machine.state}")
+        # print(f"REQUEST BODY: \n{body}")
+        if flag == True:
+            response = temp.advance(event)
+            if response == False:
+                send_text_message(event.reply_token, "Not Entering any State")
 
     return "OK"
-
+@handler.add(MessageEvent,message=TextMessage)
+def handle_message(event):
+    user_id = event.source.user_id
+    found = False
+    global first_time
+    if not first_time:
+        for item in users:
+            if item.uid == user_id:
+                found = True
+                break
+    else:
+        first_time = False
+        found = False
+    if not found:
+        new_user = User()
+        new_user.uid = user_id
+        new_user.machine = TocMachine(
+                states=["user", "state1", "state2"],
+                transitions=[
+                    {  
+                        "trigger": "advance",
+                        "source": "user",
+                        "dest": "state1",
+                        "conditions": "is_going_to_state1",
+                    },
+                    {
+                        "trigger": "advance",
+                        "source": "user",
+                        "dest": "state2",
+                        "conditions": "is_going_to_state2",
+                    },
+                    {"trigger": "go_back", "source": ["state1", "state2"], "dest": "user"},
+                ],
+                initial="user",
+                auto_transitions=False,
+                show_conditions=True,
+            )
+        users.append(new_user)
+        # print('user_id = ', user_id)
 
 # @app.route("/webhook", methods=["POST"])
 # def webhook_handler():
